2fb77740a4c0569fffb78deef35986cb35d3790f
basepd32 <basepd32@gmail.com>
2024-04-25 12:17:46 -0400

    Finish site

--- project3/main.js
+++ project3/main.js
@@ -7,12 +7,11 @@ for (var i = 0; i < breadcrumblinks.length; i++) {
   breadcrumblinks[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace("active", "");
-    this.className += " active";
+    this.className += "active";
   });
 }
 
 // hamburger menu icon interaction showing/hiding navigation links
-
 function hamburgerMenu() {
   var x = document.getElementById("hamburgerLinks");
   if (x.style.display === "block") {
